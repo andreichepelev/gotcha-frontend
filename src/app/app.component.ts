8a1eecd10c1e048cfc3520c1c176cd0ea6a83d9e
Andrei Chepelev <kirkkonen@gmail.com>
2022-10-13 15:55:19 +0300

    added api post logic 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+export interface Client {
+  email: string;
+  address: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +18,11 @@ import { FormControl, Validators } from '@angular/forms';
 export class AppComponent {
   title = 'gotcha-frontend';
   panelOpenState = false;
+  clientEndpointUrl = "https://gotcha-backend.herokuapp.com/api/add";
+
+  constructor(
+    private http: HttpClient
+  ) {}
 
   emailFormControl = new FormControl('', [
     Validators.required,
@@ -19,9 +33,21 @@ export class AppComponent {
     Validators.required,
   ]);
 
-  
-  newClient(email: string, address: string) {
-    console.log(email, address)
+
+  newClient(_email: string, _address: string) {
+
+    const newClientObj: Client = {
+      email: _email,
+      address: _address
+    };
+
+    this.http.post<Client>(this.clientEndpointUrl, newClientObj, { withCredentials: true })
+      .pipe(
+        catchError(error => {
+          console.log('Sending data failed')
+          return throwError(() => new Error('error occured'))
+        })
+      ).subscribe(newClientObj => console.log("new client added: ", newClientObj))
   }
 
 }
