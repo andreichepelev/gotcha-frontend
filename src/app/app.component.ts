a9d1ef70ff0d9acfe1991e52d8ba778813c66683
Andrei Chepelev <kirkkonen@gmail.com>
2022-10-17 10:20:04 +0300

    added headers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -19,10 +19,28 @@ export class AppComponent {
   title = 'gotcha-frontend';
   panelOpenState = false;
   clientEndpointUrl = "https://gotcha-backend.herokuapp.com/api/add";
+  // private corsHeaders: HttpHeaders;
+
 
   constructor(
     private http: HttpClient
-  ) {}
+  ) {
+    // this.corsHeaders = new HttpHeaders({
+    //   'Content-Type': 'application/json',
+    // });
+  }
+
+  headerDict = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+  }
+  
+  requestOptions = {                                                                                                                                                                                 
+    headers: new HttpHeaders(this.headerDict),
+    withCredentials: true
+  };
+  
 
   emailFormControl = new FormControl('', [
     Validators.required,
@@ -32,6 +50,28 @@ export class AppComponent {
   addressFormControl = new FormControl('', [
     Validators.required,
   ]);
+
+  // getContent(subs: Array<string>): Observable<IContent> {
+  //   return (() => {
+  //     return this.http.get<IContent>( (() => {
+  //       let r = this.root;
+  //       subs.forEach((s, i, a) => {
+  //         if(i === a.length-1) {
+  //           r += s;
+  //         }
+  //         else {
+  //           if(s !== '/') {
+  //             r += s;
+  //           }
+  //         }
+  //       });
+  //       return r;
+  //     })(), {
+  //       headers: this.corsHeaders
+  //     });
+
+  //   })();
+  // }
 
 
   newClient(_email: string, _address: string) {
@@ -43,7 +83,10 @@ export class AppComponent {
 
     console.log("object to send: ", newClientObj)
 
-    this.http.post<Client>(this.clientEndpointUrl, newClientObj, { withCredentials: true })
+    //     this.http.post<Client>(this.clientEndpointUrl, newClientObj, { withCredentials: true })
+
+
+    this.http.post<Client>(this.clientEndpointUrl, newClientObj, this.requestOptions)
       .pipe(
         catchError(error => {
           console.log('Sending data failed')
